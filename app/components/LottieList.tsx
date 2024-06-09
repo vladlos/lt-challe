f4205d75ce49ded4fb078d61facf3ccc83b4b20d
vladlos <carrott9@gmail.com>
2024-06-09 16:00:04 +0300

    allotie table fix ui

--- app/components/LottieList.tsx
+++ app/components/LottieList.tsx
@@ -4,29 +4,47 @@ import { Lottie } from '@prisma/client';
 
 type LottieListProps = {
   lotties: Lottie[];
-  actions?: (id) => React.ReactNode;
+  actions?: (id: string) => React.ReactNode;
 };
 
 const LottieList: React.FC<LottieListProps> = ({ lotties, actions }) => {
   return (
-    <ul className="mb-4">
-      {lotties.map((lottie) => (
-        <li key={lottie.id} className="border-b py-2">
-          <div className="flex justify-between">
-            <Link
-              to={`/edit/${lottie.id}/chat`}
-              className="text-indigo-600 hover:underline"
-            >
-              {lottie.name}
-            </Link>
-            <span className="text-sm text-gray-500">
-              {new Date(lottie.createdAt).toLocaleDateString()}
-              {actions && actions(lottie.id)}
-            </span>
-          </div>
-        </li>
-      ))}
-    </ul>
+    <div className="overflow-x-auto pb-4">
+      <table className="min-w-full bg-white">
+        <thead>
+          <tr>
+            <th className="py-2 px-4 border-b-2 border-gray-300 text-left leading-tight">
+              Name
+            </th>
+            <th className="py-2 px-4 border-b-2 border-gray-300 text-right leading-tight text-nowrap">
+              Created At
+            </th>
+            {actions && (
+              <th className="py-2 px-4 border-b-2 border-gray-300 text-right leading-tight">
+                Actions
+              </th>
+            )}
+          </tr>
+        </thead>
+        <tbody>
+          {lotties.map((lottie) => (
+            <tr key={lottie.id} className="border-b">
+              <td className="py-2 px-4 text-indigo-600 hover:underline w-full">
+                <Link to={`/edit/${lottie.id}/chat`}>{lottie.name}</Link>
+              </td>
+              <td className="py-2 px-4 text-gray-500 text-sm whitespace-nowrap text-right">
+                {new Date(lottie.createdAt).toLocaleDateString()}
+              </td>
+              {actions && (
+                <td className="py-2 px-4 text-gray-500 text-sm whitespace-nowrap text-right">
+                  {actions(lottie.id)}
+                </td>
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
